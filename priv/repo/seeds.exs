6a1a936b89731398cdccc8cc3196c816dc9ee9fb
jonathan bechtel <jonathan.b@fevtutor.com>
2025-02-07 16:17:02 +0530

    updating data pipeline

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -14,17 +14,33 @@ import DraftGuru.DataLoader, only: [create_dataset: 1]
 alias DraftGuru.Players
 
 defmodule DraftGuru.DraftCombineStatsSeed do
+alias ExUnit.FailuresManifest
 
   # load in player data
   def seed_data(filepath) do
     player_data = create_dataset(filepath)
+    process_player_data(player_data)
   end
 
   def process_player_data(player_data) do
     Enum.map(player_data, fn player_map ->
-        case Players.get_player_by_name(player_map) do
-          _ -> :ok
+
+      # check to see if this player exists inside the canonical table
+      # store the player information if it does
+      canonical_player_record = case Players.get_player_by_name(player_map) do
+          %Player{} = player -> {:ok, true, player}
+          nil -> {:error, false, :not_found}
         end
+
+      # if there is a canonical player record,
+      # check to see if there's a corresponding player id record
+      player_id_record = case canonical_player_record do
+        {:ok, true, player} -> case PlayerID.get_player!(player.id) do
+          %PlayerID{} = player -> {:ok, true, player_id}
+          nil -> {:error, false, :not_found}
+        end
+      end
+
     end)
   end
 
