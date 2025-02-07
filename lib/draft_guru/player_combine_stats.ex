f3a6fe2c1973c5d2eed8d6490029520afeebd7aa
jonathan bechtel <jonathan.b@fevtutor.com>
2025-02-07 18:26:21 +0530

    added another update to combine stats context

--- lib/draft_guru/player_combine_stats.ex
+++ lib/draft_guru/player_combine_stats.ex
@@ -3,15 +3,18 @@ defmodule DraftGuru.PlayerCombineStats do
   Context for the PlayerCombineStats table
   """
   import Ecto.Query, warn: false
+  alias DraftGuru.PlayerCombineStats
   alias DraftGuru.Repo
   alias DraftGuru.Players.PlayerCombineStats
 
 
   def get_player_combine_stats!(id), do: Repo.get!(PlayerCombineStats, id)
 
+  def get_player_combine_stats_by_player_id!(player_id), do: Repo.get_by!(PlayerCombineStats, player_id)
+
   def create_player_combine_stats(attrs \\ %{}) do
     %PlayerCombineStats{}
-    |> Repo.changeset(attrs)
+    |> PlayerCombineStats.changeset(attrs)
     |> Repo.insert()
   end
 end
