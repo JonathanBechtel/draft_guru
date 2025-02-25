65b2aafd97e4a8683bd371587bfb61b900572169
jonathan bechtel <jonathan.b@fevtutor.com>
2025-02-25 13:41:41 -0500

    updated player combine search

--- lib/draft_guru/player_combine_stats.ex
+++ lib/draft_guru/player_combine_stats.ex
@@ -8,11 +8,13 @@ defmodule DraftGuru.PlayerCombineStats do
 
   def list_players_combine_stats(params) do
 
-    player_slug = Map.get(params, "player_slug")
+    player_slug = Map.get(params, "player_combine_stats_search")
 
     query = PlayerCombineStat
 
     query = maybe_apply_search(query, player_slug)
+
+    _page = to_integer_with_default(Map.get(params, "page"), 1)
 
     Repo.all(query)
 
@@ -23,7 +25,15 @@ defmodule DraftGuru.PlayerCombineStats do
   defp maybe_apply_search(query, player_slug) do
       from(p in query,
           where:
-            ilike(p.player_slug, ^"%#{idlookup}%"))
+            ilike(p.player_slug, ^"%#{player_slug}%"))
+  end
+
+  defp to_integer_with_default(nil, default), do: default
+  defp to_integer_with_default(str, default) do
+    case Integer.parse(to_string(str)) do
+      {int, _} -> int
+      :error   -> default
+    end
   end
 
   def get_player_combine_stats!(id), do: Repo.get!(PlayerCombineStat, id)
