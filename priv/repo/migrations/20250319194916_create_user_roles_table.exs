851d89ed2eb68151a9951170dff4385d9f848f7a
jonathan bechtel <jonathanbechtel@gmail.com>
2025-03-19 20:27:09 -0400

    fixed schema issues in ecto

--- priv/repo/migrations/20250319194916_create_user_roles_table.exs
+++ priv/repo/migrations/20250319194916_create_user_roles_table.exs
@@ -2,10 +2,12 @@ defmodule DraftGuru.Repo.Migrations.CreateUsersRolesTable do
   use Ecto.Migration
 
   def change do
-    create table(:users_roles) do
+    create table(:user_roles) do
       add :role, :string, null: false
 
       timestamps(utc: :datetime)
     end
+
+    create unique_index(:user_roles, [:role])
   end
 end
